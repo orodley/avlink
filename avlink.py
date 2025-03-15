45ac40e9af2b818addd47d9d4793addf11c3521c
Owen Rodley <owen.rodley@gmail.com>
2025-03-15 22:33:50 +1100

    Extract link targets from the ToC

--- avlink.py
+++ avlink.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import re
 import sys
+from pprint import pprint as pp
 
 import fitz
 
@@ -16,9 +18,51 @@ def main(args):
     if not toc:
         exit(f"No table of contents found in {input_filename}")
 
-    for (level, title, page_num, *_) in toc:
+    link_targets = {}
+    for (_, title, page_num, *_) in toc:
         title = title.strip()
-        print(f"{(level - 1) * ' '}'{title}' {page_num}")
+        short_name = extract_short_name(title)
+        if short_name:
+            link_targets[short_name] = page_num
+    pp(link_targets)
+    print(f"{len(link_targets)} targets found")
+
+
+def extract_short_name(title):
+    # For now this is just area keys.
+    #
+    # Area keys all have the format: "X-n: Name", where X is the short name for
+    # the level and n is the room/area number.
+    #
+    # Level names are usually just a number, or "SLn" for sublevels, and a few
+    # other special cases like "AV" for the ruined city.
+    #
+    # Room/area numbers are just numbers, up to three digits.
+    #
+    # Both level names and room/area numbers sometimes have a capital letter
+    # suffix, like "SL10A" or "187B".
+    match = re.match(
+        r"""(?x)
+        ^(
+           (
+             \d{1,2}         |
+             SL\d{1,2}[A-Z]? |
+             AV|EX|UP|TS
+           )-(\d{1,3})[A-Z]?
+        ):.*$""",
+        title,
+    )
+
+    # TODO:  Extract other stuff:
+    #   * Monsters
+    #   * Items
+    #   * Flora
+    #   * Spells
+    #   * Books
+    #   * NPCs
+
+    if match:
+        return match.group(1)
 
 
 def exit(message):
